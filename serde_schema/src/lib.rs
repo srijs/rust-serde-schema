c7337632f79df2e4fa11c1e57a02fe524e180a29
Sam Rijs <srijs@airpost.net>
2018-05-23 20:27:01 +1000

    initial commit

---
+++ serde_schema/src/lib.rs
@@ -0,0 +1,17 @@
+#![deny(warnings)]
+
+extern crate serde;
+
+#[cfg(feature = "bytes")]
+extern crate serde_bytes;
+
+pub mod types;
+
+mod schema;
+pub use self::schema::Schema;
+
+mod serializer;
+pub use self::serializer::SchemaSerializer;
+
+mod serialize;
+pub use self::serialize::SchemaSerialize;
